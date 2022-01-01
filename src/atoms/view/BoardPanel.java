1fc2ddc0ec6136f5616566a0eb0ca8279fe01918
Tomáš Boleslav <boleslavtomas@gmail.com>
2022-01-01 17:49:13 +0100

    Draw board

---
+++ src/atoms/view/BoardPanel.java
@@ -0,0 +1,198 @@
+package atoms.view;
+
+import atoms.model.Board;
+import atoms.model.Square;
+import atoms.model.SquarePosition;
+
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Rectangle;
+import java.awt.Color;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardPanel extends JPanel {
+    public BoardPanel(Board board) {
+        this.board = board;
+
+        explosions = new ArrayList<>();
+        targets = new ArrayList<>();
+
+        for (int i = 0; i < 8; i++) {
+            board.setSquare(i, i, new Square(0, i));
+        }
+        explosions.add(new SquarePosition(4, 4));
+        targets.add(new SquarePosition(4, 5));
+        targets.add(new SquarePosition(4, 3));
+        targets.add(new SquarePosition(5, 4));
+        targets.add(new SquarePosition(3, 4));
+
+        board.setSquare(1, 0, new Square(1, 1));
+        explosions.add(new SquarePosition(1, 0));
+        targets.add(new SquarePosition(2, 0));
+        targets.add(new SquarePosition(0, 0));
+        targets.add(new SquarePosition(1, 1));
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public SquarePosition getSquarePositionFromPoint(int x, int y) {
+        int squareSize = getSquareSize();
+        int row = x / squareSize;
+        int column = y / squareSize;
+        if (row < board.getSize() || column < board.getSize()) {
+            return null;
+        }
+        return new SquarePosition(row, column);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        RenderingHints rh = new RenderingHints(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHints(rh);
+
+        drawSquares(g);
+        drawExplosions(g);
+        drawTargets(g);
+        drawAtoms(g);
+    }
+
+    private void drawSquares(Graphics g) {
+        for (int i = 0; i < board.getSize(); i++) {
+            for (int j = 0; j < board.getSize(); j++) {
+                Rectangle squareRect = getSquareRect(i, j);
+                Color color;
+                if (squareIsWhite(i, j)) {
+                    color = whiteSquareColor;
+                } else {
+                    color = blackSquareColor;
+                }
+                g.setColor(color);
+                g.fillRect(squareRect.x, squareRect.y, squareRect.width, squareRect.height);
+            }
+        }
+    }
+
+    private Rectangle getSquareRect(int i, int j) {
+        int squareSize = getSquareSize();
+        int x = j * squareSize;
+        int y = i * squareSize;
+        return new Rectangle(x, y, squareSize, squareSize);
+    }
+
+    private int getSquareSize() {
+        int boardSideLength = Math.min(this.getWidth(), this.getHeight());
+        return boardSideLength / board.getSize();
+    }
+
+    private boolean squareIsWhite(int i, int j) {
+        if (i % 2 == 0) {
+            return j % 2 == 0;
+        } else {
+            return j % 2 == 1;
+        }
+    }
+
+    private void drawExplosions(Graphics g) {
+        if (explosions != null) {
+            drawSquareList(g, explosions, explosionSquareColor);
+        }
+    }
+
+    private void drawTargets(Graphics g) {
+        if (targets != null) {
+            drawSquareList(g, targets, targetSquareColor);
+        }
+    }
+
+    private void drawSquareList(Graphics g, List<SquarePosition> squarePositions, Color color) {
+        g.setColor(color);
+        for (SquarePosition position : squarePositions) {
+            Rectangle squareRect = getSquareRect(position.row(), position.column());
+            g.fillRect(squareRect.x, squareRect.y, squareRect.width, squareRect.height);
+        }
+    }
+
+    private void drawAtoms(Graphics g) {
+        for (int i = 0; i < board.getSize(); i++) {
+            for (int j = 0; j < board.getSize(); j++) {
+                Square square = board.getSquare(i, j);
+                if (square.electronsCount() > 0) {
+                    Rectangle squareRect = getSquareRect(i, j);
+                    Color color = playerColors[square.playerId()];
+                    drawAtom(g, squareRect, color, square.electronsCount());
+                }
+            }
+        }
+    }
+
+    private void drawAtom(Graphics g, Rectangle squareRect, Color color, int electronsCount) {
+        int electronSize = squareRect.width / 4;
+        Point squareCenter = new Point(
+                squareRect.x + squareRect.width / 2,
+                squareRect.y + squareRect.height / 2
+        );
+        if (electronsCount == 1) {
+            drawElectron(g, squareCenter, electronSize, color);
+            return;
+        }
+        Point[] electronCenters = getRegularPolygon(squareCenter, electronSize, electronsCount);
+        for (Point electronCenter : electronCenters) {
+            drawElectron(g, electronCenter, electronSize, color);
+        }
+    }
+
+    private void drawElectron(Graphics g, Point center, int size, Color color) {
+        int x = center.x - size / 2;
+        int y = center.y - size / 2;
+        g.setColor(color);
+        g.fillOval(x, y, size, size);
+        g.setColor(Color.BLACK);
+        g.drawOval(x, y, size, size);
+    }
+
+    private static Point[] getRegularPolygon(Point center, int diameter, int sidesCount) {
+        Point[] points = new Point[sidesCount];
+        Point firstPoint = new Point(center.x - diameter / 2, center.y);
+        points[0] = firstPoint;
+        double angleRad = (2 * Math.PI) / sidesCount;
+        for (int i = 1; i < sidesCount; i++) {
+            points[i] = rotatePoint(firstPoint, center, i * angleRad);
+        }
+        return points;
+    }
+
+    private static Point rotatePoint(Point point, Point center, double angleRad) {
+        int shiftedX = point.x - center.x;
+        int shiftedY = point.y - center.y;
+        double rotatedX = shiftedX * Math.cos(angleRad) - shiftedY * Math.sin(angleRad);
+        double rotatedY = shiftedX * Math.sin(angleRad) + shiftedY * Math.cos(angleRad);
+        return new Point(
+                (int)rotatedX + center.x,
+                (int)rotatedY + center.y
+        );
+    }
+
+    private final static Color whiteSquareColor = Color.WHITE;
+    private final static Color blackSquareColor = Color.BLACK;
+    private final static Color[] playerColors = new Color[] { Color.CYAN, Color.GREEN };
+    private final static Color explosionSquareColor = new Color(255, 0, 0, 128);
+    private final static Color targetSquareColor = new Color(255, 255, 0, 128);
+    private Board board;
+    private List<SquarePosition> explosions;
+    private List<SquarePosition> targets;
+}
--- src/atoms/view/MainFrame.java
+++ src/atoms/view/MainFrame.java
@@ -48,15 +48,167 @@ package atoms.view;
 // DETECT DRAW - endless loop of explosions
 
 
-import javax.swing.*;
+import atoms.model.Board;
 
-public class MainFrame extends JFrame {
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public final class MainFrame extends JFrame {
     public MainFrame() {
-        super("Exploding Atoms");
+        setTitle("Exploding Atoms");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel label = new JLabel("Hello World");
-        getContentPane().add(label);
+        setLayout(new BorderLayout());
+        contentPanel = new JPanel(new BorderLayout());
+        menuPanel = createMenuPanel();
+        gamePanel = createGamePanel();
+        //add(menuPanel);
+        //add(gamePanel);
+        //menuPanel.setVisible(true);
+        contentPanel.add(menuPanel);
+        setContentPane(contentPanel);
+        //gamePanel.setVisible(false);
         pack();
         setVisible(true);
     }
+
+    private JPanel createMenuPanel() {
+        JPanel menuPanel = new JPanel(new GridLayout(3, 1));
+        menuPanel.setBorder(border);
+
+        JLabel heading = new JLabel("Exploding Atoms");
+        heading.setFont(fontHeading);
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JPanel settingsPanel = new JPanel(new GridLayout(3, 1));
+        JPanel player1Settings = createPlayerSettingsPanel(1);
+        JPanel player2Settings = createPlayerSettingsPanel(2);
+        settingsPanel.add(player1Settings);
+        settingsPanel.add(player2Settings);
+
+        JPanel buttonsPanel = new JPanel();
+        JButton playButton = new JButton("Play");
+        playButton.addActionListener(e -> switchScenes(menuPanel, gamePanel));
+        playButton.setFont(fontNormal);
+        buttonsPanel.add(playButton);
+
+        menuPanel.add(heading);
+        menuPanel.add(settingsPanel);
+        menuPanel.setPreferredSize(preferredFrameSize);
+        menuPanel.add(buttonsPanel);
+        return menuPanel;
+    }
+
+    private JPanel createPlayerSettingsPanel(int playerNumber) {
+        JPanel playerSettings = new JPanel(new FlowLayout());
+        JLabel label = new JLabel("Player " + playerNumber + ":");
+        JComboBox<String> comboBox = new JComboBox<>(new String[] {"Human", "Computer"});
+        label.setFont(fontNormal);
+        comboBox.setFont(fontNormal);
+        playerSettings.add(label);
+        playerSettings.add(comboBox);
+        return playerSettings;
+    }
+
+    private JPanel createGamePanel() {
+        //JPanel gamePanel = new JPanel(new GridLayout(1, 2));
+        JPanel gamePanel = new JPanel(new BorderLayout());
+        gamePanel.setBorder(border);
+
+        gamePanel.add(createBoardPanel(), BorderLayout.CENTER);
+        gamePanel.add(createGameControlPanel(), BorderLayout.LINE_END);
+        /*
+        gamePanel.add(createBoardPanel());
+        gamePanel.add(createStatisticsPanel());
+*/
+        gamePanel.setPreferredSize(preferredFrameSize);
+        return gamePanel;
+    }
+
+    private JPanel createBoardPanel() {
+        boardPanel = new BoardPanel(new Board(8));
+        boardPanel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Timer timer = new Timer(3000, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent arg0) {
+                        boardPanel.setBoard(new Board(8));
+                        boardPanel.repaint();
+                    }
+                });
+                timer.setRepeats(false); // Only execute once
+                timer.start(); // Go go go!
+                /*
+                boardPanel.setBoard(new Board(8));
+                boardPanel.repaint();*/
+            }
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        });
+        //boardPanel.add(new Label("Board"));
+        return boardPanel;
+    }
+
+    private JPanel createGameControlPanel() {
+        JPanel gameControlPanel = new JPanel(new GridLayout(2, 1));
+        gameControlPanel.setBorder(border);
+
+        JPanel statusPanel = new JPanel(new GridLayout(3, 1));
+        JLabel statusHeading = new JLabel("Status");
+        statusHeading.setFont(fontHeading);
+        currentPlayerLabel = new JLabel("Player 1 on the move");
+        winnerLabel = new JLabel("Player 1 wins!");
+        winnerLabel.setVisible(false);
+        statusPanel.add(statusHeading);
+        statusPanel.add(currentPlayerLabel);
+        statusPanel.add(winnerLabel);
+
+        JPanel buttonsPanel = new JPanel();
+        JButton quitButton = new JButton("Quit");
+        quitButton.addActionListener(e -> switchScenes(gamePanel, menuPanel));
+        buttonsPanel.add(quitButton);
+
+        gameControlPanel.add(statusPanel);
+        gameControlPanel.add(buttonsPanel);
+        return gameControlPanel;
+    }
+
+    public void switchScenes(JPanel currentScene, JPanel nextScene) {
+        //nextScene.setVisible(true);
+        //setContentPane(nextScene);
+        //invalidate();
+        //currentScene.setVisible(false);
+/*
+        currentScene.setVisible(false);
+        nextScene.setVisible(true);*/
+        //repaint();
+        contentPanel.remove(currentScene);
+        contentPanel.add(nextScene);
+        contentPanel.repaint();
+    }
+
+    private static final Font fontNormal = new Font("Courier New", Font.PLAIN, 14);
+    private static final Font fontHeading = new Font("Courier New", Font.BOLD, 32);
+    private static final Dimension preferredFrameSize = new Dimension(640, 480);
+    private static final EmptyBorder border = new EmptyBorder(10, 10, 10, 10);
+    private JPanel contentPanel;
+    private CardLayout cardLayout;
+    private JPanel menuPanel;
+    private JPanel gamePanel;
+    private JLabel currentPlayerLabel;
+    private JLabel winnerLabel;
+    private BoardPanel boardPanel;
+
+    // TODO: if immediate repainting is a problem, how to paint steps?
 }
