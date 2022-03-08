1b5feac70e7526efa0ee03e1a0eb5d6e448d5838
Tomáš Boleslav <boleslavtomas@gmail.com>
2022-03-09 00:16:08 +0100

    Add comments for board state

--- src/atoms/model/BoardState.java
+++ src/atoms/model/BoardState.java
@@ -11,12 +11,28 @@ package atoms.model;
 // - explosion targets are yellow
 // - squares can have up to 7 circles
 
+/**
+ * The state of the game board.
+ */
 public class BoardState {
+
+    /**
+     * Creates a board state caused by a move.
+     * @param board The board.
+     * @param origin The target position that was the cause of this state.
+     * @param electronCounts The number of electrons of each player.
+     */
     public BoardState(Board board, SquarePosition origin, int[] electronCounts) {
         this.board = board;
         this.origin = origin;
         this.electronCounts = electronCounts;
     }
+
+    /**
+     * Creates an initial board state.
+     * @param boardSize The size of the board.
+     * @param playersCount The number of players.
+     */
 
     public BoardState(int boardSize, int playersCount) {
         board = new Board(boardSize);
@@ -24,6 +40,10 @@ public class BoardState {
         electronCounts = new int[playersCount];
     }
 
+    /**
+     * Checks if all players have made a move.
+     * @return An indicator whether all players have made a move.
+     */
     public boolean allPlayersMoved() {
         int totalElectronsCount = 0;
         for (int playerElectrons : electronCounts) {
@@ -32,6 +52,10 @@ public class BoardState {
         return totalElectronsCount >= electronCounts.length;
     }
 
+    /**
+     * Checks whether the state is terminal (a player won).
+     * @return An indicator whether the state is terminal (a player won).
+     */
     public boolean isTerminal() {
         int totalElectronsCount = 0;
         int playersAlive = 0;
@@ -47,34 +71,35 @@ public class BoardState {
         return totalElectronsCount >= electronCounts.length;
     }
 
-    public BoardState copy() {
-        return new BoardState(
-                board.copy(),
-                origin,
-                electronCounts.clone()
-        );
-    }
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-
+    /**
+     * Gets the target square of the move that caused this state.
+     * @return The target square of the move that caused this state.
+     */
     public SquarePosition getTarget() {
         return origin;
     }
 
+    /**
+     * Gets the number of electrons of a player.
+     * @param playerId The ID of the player.
+     * @return The number of electrons of the player.
+     */
     public int getElectronsCount(int playerId) {
         return electronCounts[playerId];
     }
 
+    /**
+     * Gets the number of electrons of each player.
+     * @return The number of electrons of each player.
+     */
     public int[] getAllElectronCounts() {
         return electronCounts;
     }
 
+    /**
+     * Gets the board.
+     * @return The board.
+     */
     public Board getBoard() {
         return board;
     }
@@ -82,5 +107,4 @@ public class BoardState {
     private final Board board;
     private final SquarePosition origin;
     private final int[] electronCounts;
-    private int value;
 }
