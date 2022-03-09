67943e7cd5702dacd776615993bffd63532469fa
Tomáš Boleslav <boleslavtomas@gmail.com>
2022-03-09 02:35:40 +0100

    Add comments to Square

--- src/atoms/model/Square.java
+++ src/atoms/model/Square.java
@@ -1,3 +1,8 @@
 package atoms.model;
 
+/**
+ * Square on the game board.
+ * @param playerId The ID of the player that owns the square.
+ * @param electronsCount The number of electrons on the square.
+ */
 public record Square(int playerId, int electronsCount) {}
