bf7908defb8be9b050e6b74ba3d7ae0d49dec03a
Tomáš Boleslav <boleslavtomas@gmail.com>
2022-03-12 18:29:33 +0100

    Write more information about packages

--- src/main/java/atoms/model/package-info.java
+++ src/main/java/atoms/model/package-info.java
@@ -1,5 +1,18 @@
 /**
- * This package is responsible for the main logic of the game and also contains
- * data structures representing parts of the game and the game itself.
+ * Contains the main logic of the game and related data structures.
+ * 
+ * A board is a two-dimensional array of squares, where each square can be
+ * owned by a player and contain electrons.
+ * 
+ * A state of the game is represented by BoardState. These states also represent
+ * possible moves of a player.
+ * In order for the View to have more possibilities when displaying moves,
+ * a DetailedMove is returned instead of a BoardState. It separates the move
+ * into phases of explosions that happen at the same time.
+ * 
+ * The algorithm used to choose a move for an A. I. player is minimax with
+ * alpha-beta pruning. The value of a move is the difference between the number
+ * of electrons of both players. If there are more moves  with the same value,
+ * one is chosen randomly.
  */
 package atoms.model;
--- src/main/java/atoms/view/package-info.java
+++ src/main/java/atoms/view/package-info.java
@@ -1,7 +1,16 @@
 /**
  * Contains UI components and event handling.
  *
- * Uses the game model for the representation of the game and necessary
- * computation and logic.
+ * This package uses the Java Swing library to provide a user interface.
+ * Components are created in the code manually without any configuration files.
+ *
+ * The game model package is used for the logic of the game and computation.
+ *
+ * Moves of players are displayed sequentially in phases of explosions that
+ * happen at the same time. Currently, the delays between phases are blocking
+ * and prevent event handling during the time when a move is being displayed.
+ * This can cause the window to freeze if there are too many explosions.
+ * A better solution could be using timers to dispatch events that are handled
+ * after a fixed time interval. 
  */
 package atoms.view;
